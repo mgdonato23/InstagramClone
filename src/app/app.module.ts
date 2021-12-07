87cc87c63b936c11a1fcceb178da7a64b13c85ff
mgdonato23 <mg.donato23@gmail.com>
2021-12-06 21:59:02 -0300

    Seção 24: Instagram Clone - Primeiros Passos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcessoComponent } from './acesso/acesso.component';
+import { BannerComponent } from './acesso/banner/banner.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+import { LoginComponent } from './acesso/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    AcessoComponent,
+    BannerComponent,
+    CadastroComponent,
+    LoginComponent
+   ],
   imports: [
     BrowserModule
   ],
