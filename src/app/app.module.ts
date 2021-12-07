b07b37a9a6843f412020cc6fbbc49e5c2498694a
mgdonato23 <mg.donato23@gmail.com>
2021-12-06 21:33:53 -0300

    Subir tudo que mudou

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AcessoComponent } from './acesso/acesso.component';
-import { BannerComponent } from './acesso/banner/banner.component';
-import { LoginComponent } from './acesso/login/login.component';
-import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AcessoComponent,
-    BannerComponent,
-    LoginComponent,
-    CadastroComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
